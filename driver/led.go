571b680be80807177b951e3603626f23f726a79c
kedarkhetia <kedarkhetia@gmail.com>
2019-11-26 20:19:23 -0800

    Added changes for LED Device

--- driver/led.go
+++ driver/led.go
@@ -1,8 +1,6 @@
 package driver
 
 import (
-	"encoding/json"
-	"fmt"
 	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
 	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 	"github.com/stianeikeland/go-rpio"
@@ -13,48 +11,32 @@ type Led struct {
 	DefaultDeviceProfile
 }
 
-type Pin struct {
-	Addr int	`json:"addr"`
-	State bool `json:"state"`
-}
-
-var Pins map[int]bool
+var pin = 17
+var state = false
 
 func init() {
-	Pins = make(map[int]bool)
-	Pins[17] = false
-	setPinLow(17)
+	setPinLow(pin)
 }
 
 func (l *Led) get(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest) (res []*dsModels.CommandValue, err error) {
 	now := time.Now().UnixNano()
 	res = make([]*dsModels.CommandValue, 1)
-	states, err := json.Marshal(Pins)
-	if err == nil {
-		fmt.Println(string(states))
-		res[0] = dsModels.NewStringValue(reqs[0].DeviceResourceName, now, string(states))
-	} else {
-		res[0] = dsModels.NewStringValue(reqs[0].DeviceResourceName, now, "")
-	}
-	return
+	res[0], _ = dsModels.NewBoolValue(reqs[0].DeviceResourceName, now, state)
 }
 
 func (l *Led) set(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest, params []*dsModels.CommandValue) error {
-	if params[0].DeviceResourceName == "states" {
-		if value, err := params[0].StringValue(); err == nil {
-			pin := Pin{}
-			_ = json.Unmarshal([]byte(value), &pin)
-			led := rpio.Pin(pin.Addr) // pin for light sensor
+	if params[0].DeviceResourceName == "led" {
+		if value, err := params[0].BoolValue(); err == nil {
+			led := rpio.Pin(pin) // pin for light sensor
 			err := rpio.Open()
 			if err == nil {
 				led.Output()
-				if pin.State {
+				if value {
 					led.High()
 				} else {
 					led.Low()
 				}
-				fmt.Println(pin)
-				Pins[pin.Addr] = pin.State
+				state = value
 				rpio.Close()
 			}
 		}
