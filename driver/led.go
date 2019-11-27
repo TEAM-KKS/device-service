e2dd7faa5ca2f0d6e79ffeb33ed35e95606bcebc
kedarkhetia <kedarkhetia@gmail.com>
2019-11-27 01:53:39 -0800

    Added locks for hardware

--- driver/led.go
+++ driver/led.go
@@ -4,11 +4,13 @@ import (
 	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
 	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 	"github.com/stianeikeland/go-rpio"
+	"sync"
 	"time"
 )
 
 type Led struct {
 	DefaultDeviceProfile
+	mux sync.Mutex
 }
 
 var pin = 17
@@ -19,6 +21,8 @@ func init() {
 }
 
 func (l *Led) get(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest) (res []*dsModels.CommandValue, err error) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
 	now := time.Now().UnixNano()
 	res = make([]*dsModels.CommandValue, 1)
 	res[0], _ = dsModels.NewBoolValue(reqs[0].DeviceResourceName, now, state)
@@ -26,7 +30,9 @@ func (l *Led) get(deviceName string, protocols map[string]contract.ProtocolPrope
 }
 
 func (l *Led) set(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest, params []*dsModels.CommandValue) error {
-	value, err := params[0].BoolValue();
+	l.mux.Lock()
+	defer l.mux.Unlock()
+	value, err := params[0].BoolValue()
 	if err == nil {
 		err := rpio.Open()
 		if err == nil {
--- driver/lightsensor.go
+++ driver/lightsensor.go
@@ -5,15 +5,19 @@ import (
 	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
 	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 	"github.com/stianeikeland/go-rpio"
+	"sync"
 	"time"
 	"os"
 )
 
 type LightSensor struct {
 	DefaultDeviceProfile
+	mux sync.Mutex
 }
 
 func (r *LightSensor) get(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest) (res []*dsModels.CommandValue, err error) {
+	r.mux.Lock()
+	defer r.mux.Unlock()
 	err = rpio.Open()
 	now := time.Now().UnixNano()
 	count := 0
